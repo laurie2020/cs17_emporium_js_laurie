b9c6cd6b6554b42912193b9856a19c7c4d15484b
Laurie <laurieiula2016@gmail.com>
2021-03-18 10:26:18 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,66 @@
+// bouton noir/blanc
+let btnWhite = document.querySelector('.white');
+let btnBlack = document.querySelector('.black');
+let body = document.querySelector('body');
+btnWhite.addEventListener('click', () => {
+    body.style.backgroundColor = "white";
+})
+btnBlack.addEventListener('click', () => {
+    body.style.backgroundColor = "black";
+})
+// fin bouton noir/blanc
+// section signup
+let connexion = document.querySelector('#connexion');
+let signup = document.querySelector('.sign-up-parent');
+let close = document.querySelector('#close');
+connexion.addEventListener('click', () => {
+    signup.classList.remove('d-none');
+})
+close.addEventListener('click', () => {
+    signup.classList.add('d-none');
+})
+// fin section signup
+// carousel
+let cards = document.querySelectorAll('.carousel-card');
+let indicators = document.querySelectorAll('#slider-indicators li');
+
+indicators.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        cards.forEach((e) => {
+            e.style.transition = "transform 0.8s ease-in-out"
+            if(window.matchMedia("(max-width: 400px)")){
+                e.style.transform = "translateX(" + (-210*index) + "px)";
+            }else if(window.matchMedia("(max-width: 1024px)")){
+                e.style.transform = "translateX(" + (-220*index*3) + "px)";
+                indicators[3].remove();
+            }else{
+                e.style.transform = "translateX(" + (-220*index) + "px)";
+            }
+            
+        });
+    })
+});
+
+// fin carousel
+
+// navbar
+let navbar = document.querySelector('header');
+window.addEventListener('scroll', ()=> {
+    if(window.scrollY >= 60 ){
+        navbar.style.paddingTop = "0";
+        navbar.style.height = "50px";
+        navbar.style.position = "fixed";
+        navbar.style.width = "100%";
+        navbar.style.display = "flex";
+        navbar.style.backgroundColor = "white";
+        navbar.style.zIndex = "20";
+    }else {
+        navbar.style.paddingTop = "50px";
+        navbar.style.height = "217px";
+        navbar.style.position = "static";
+        navbar.style.width = "100%";
+        navbar.style.display = "block";
+        navbar.style.backgroundColor = "white";
+    }
+})
+// fin navbar
